67db55e550adb71a35dbadf69ae3f99fe0845f40
Samaan Kamal <samaankamal72@gmail.com>
2025-01-20 15:06:42 +0200

    removed unnecessary imports

--- src/main/java/com/example/ecommerce/Service/Category/CategoryService.java
+++ src/main/java/com/example/ecommerce/Service/Category/CategoryService.java
@@ -1,14 +1,8 @@
 package com.example.ecommerce.Service.Category;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
 import java.util.List;
 
-import com.example.ecommerce.DB.DatabaseUtil;
 import com.example.ecommerce.Model.Category;
 
 public interface CategoryService {
