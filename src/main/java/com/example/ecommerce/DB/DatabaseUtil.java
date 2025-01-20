6633d29bec0b60ae19290a9686271a45102f9691
Samaan Kamal <samaankamal72@gmail.com>
2025-01-20 16:04:27 +0200

    changed the reading way of properties file and reomved unnecessary code

--- src/main/java/com/example/ecommerce/DB/DatabaseUtil.java
+++ src/main/java/com/example/ecommerce/DB/DatabaseUtil.java
@@ -1,24 +1,17 @@
 package com.example.ecommerce.DB;
 
-import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
 
 public class DatabaseUtil {
-	private static final String PROPERTIES_FILE = "src/main/resources/env.properties";
-//	private static final String URL = "jdbc:mysql://localhost:3306/ecommerce";
-//    private static final String USER = "root";
-//    private static final String PASSWORD = "password";
     
-    
-
     public static Connection getConnection() throws SQLException {
     	Properties properties = loadProperties();
 
-        // Fetch database connection details from properties
         String URL = properties.getProperty("DB_URL", "jdbc:mysql://localhost:3306/ecommerce"); // Default value if not set
         String USER = properties.getProperty("DB_USER", "root"); // Default value
         String PASSWORD = properties.getProperty("DB_PASSWORD", "password"); // Default value
@@ -34,13 +27,16 @@ public class DatabaseUtil {
     private static Properties loadProperties() throws SQLException {
         Properties properties = new Properties();
 
-        // Load the properties file
-        try (FileInputStream fis = new FileInputStream(PROPERTIES_FILE)) {
-            properties.load(fis);
+        try (InputStream inputStream = DatabaseUtil.class.getClassLoader().getResourceAsStream("env.properties")) {
+            if (inputStream == null) {
+                throw new SQLException("Unable to find env.properties in the classpath");
+            }
+            properties.load(inputStream);
         } catch (IOException e) {
             throw new SQLException("Unable to load properties file", e);
         }
-
+        
+        
         return properties;
     }
 }
